de9b4e11eadd63d2ca5b07ded145ab421e409e69
Türkay DALAN <turkaydalan@gmail.com>
2013-08-23 01:49:21 +0300

    Update jquery.timeago.tr.js
    Locales

--- locales/jquery.timeago.tr.js
+++ locales/jquery.timeago.tr.js
@@ -1,16 +1,20 @@
-// Turkish
-jQuery.extend($.timeago.settings.strings, {
-   suffixAgo: 'önce',
-   suffixFromNow: null,
-   seconds: '1 dakikadan',
-   minute: '1 dakika',
-   minutes: '%d dakika',
-   hour: '1 saat',
-   hours: '%d saat',
-   day: '1 gün',
-   days: '%d gün',
-   month: '1 ay',
-   months: '%d ay',
-   year: '1 yıl',
-   years: '%d yıl'
-});
+// Turkish (Template)
+jQuery.timeago.settings.strings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: "önce",
+  suffixFromNow: "bundan",
+  seconds: "en az bir dakika",
+  minute: "Yaklaşık bir dakika",
+  minutes: "%d dakika",
+  hour: "yaklaşık bir saat",
+  hours: "yaklaşık %d saat",
+  day: "bir gün",
+  days: "%d gün",
+  month: "yaklaşık bir ay",
+  months: "%d ay",
+  year: "yaklaşık bir yıl",
+  years: "%d yıl",
+  wordSeparator: " ",
+  numbers: []
+};
